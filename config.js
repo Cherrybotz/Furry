43c0f0b2fefdee48d064ed8e964ab3a99174024e
Cherry <109952722+Cherrybotz@users.noreply.github.com>
2022-09-27 09:06:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some h
 global.thumbt = global.img
 global.fsx = 999999999999
 global.kontak = [
-['62895627514070', 'Ray San', true], 
-['6281779211748', 'YUKINO-MD', true] 
+['94726083178', 'Lakshan', true], 
+['1336739', 'Mitsuha-MD', true] 
 ]
 //Lolkey
 global.lolkey = 'sonelganz'
@@ -57,17 +57,17 @@ global.APIKeys = {
  'https://zenzapis.xyz' : 'f9fccfcff1'
 }
 
-global.linkyt = 'https://youtu.be/60yZO4UQKdI'
-global.WM = '𝑴𝒚𝑩𝒐𝒕-𝑴𝒖𝒍𝒕𝒊 𝑫𝒆𝒗𝒊𝒄𝒆 𝑩𝒚 𝚃𝚑𝚎.𝚂𝚊𝚍.𝙱𝚘𝚢𝟶𝟷'
-global.linkgc = 'https://chat.whatsapp.com/CZlamemhpn06a5sW063xOg'
-global.linkig = 'https://www.instagram.com/the.sad.boy01'
+global.linkyt = 'https://youtu.be/oppositers'
+global.WM = 'lakshan'
+global.linkgc = 'https://chat.whatsapp.com/FKB3BjBGMYjBJZfJXjEAOa'
+global.linkig = 'https://www.instagram.com/lakshan'
 global.deslink = ''
 global.logo = 'https://telegra.ph/file/1665521d176fbf172d735.jpg'
 global.titlink = 'Folllow me on Instagram'
-global.bodlink = '@the.sad.boy01'
+global.bodlink = '@lakshan'
 
-global.wait = '_*𝐬𝐞𝐝𝐚𝐧𝐠 𝐝𝐢 𝐩𝐫𝐨𝐬𝐞𝐬 ...*_'
-global.eror = '_*ᴍᴀᴀғ sᴇʀᴠᴇʀ ᴛɪᴅᴀᴋ sᴛᴀʙɪʟ..*_'
+global.wait = '_*Wait a minute honey,your request is processing...*_'
+global.eror = '_*Honey! It is m*f server error..*_'
 
 //========Url Template Buttons==========//
 global.dtu = '🌏 GROUP OFFICIAL'
@@ -75,7 +75,7 @@ global.urlnya = global.linkgc
 
 //============= callButtons ============//
 global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '+62 895-6275-14070'
+global.phn = '+94726083178'
 
 //========== Tampilan Bot =============//
 global.sa = '╭─'
